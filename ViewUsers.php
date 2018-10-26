82944b84053e434ea5eab13f2e5512091e129953
Ayah Alkhatib <ayah.alkhatib90@gmail.com>
2018-10-26 01:40:55 -0500

    part 4 and part 5 have been completed

---
+++ ViewUsers.php
@@ -0,0 +1,15 @@
+<?php
+  $sql = new mysqli ("mysql.eecs.ku.edu", "a769a226", "main4gae", "a769a226");
+  $users = mysqli_query($sql, "SELECT * FROM Users");
+  $no = 1;
+  while($i = mysqli_fetch_array($users)){
+	echo "<tr>";
+	  echo "<td>";
+	    echo $no . ". " . $i['user_id'];
+	    $no = $no + 1;
+	  echo "</td>";
+	  echo "<br>";
+	echo "</tr>";
+  }
+  $sql->close();
+?>
